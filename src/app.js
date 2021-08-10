e42bdee36a97770431e187691c14b1af9110b5ff
Simone Amorim <simoneas03@gmail.com>
2021-08-09 21:15:06 -0300

    Extract amount performance for plays from into statement function.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const invoices = require('./data/invoices.json')
 const plays = require('./data/plays.json')
 
-import { statment } from './01/statment.js'
+import { statment } from './02/statment.js'
 
 statment(invoices, plays)
