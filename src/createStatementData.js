8f67e71d13806bd0962b510637d46e36deb6034c
Simone Amorim <simoneas03@gmail.com>
2021-12-01 08:56:02 -0300

    Move statementData into to a separate file

---
+++ src/createStatementData.js
@@ -0,0 +1,67 @@
+export const createStatementData = (invoice, plays) => {
+  const playFor = aPerformance => plays[aPerformance.playID]
+
+  const amountFor = invoice => {
+    let result = 0
+    switch (invoice.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (invoice.audience > 30) {
+          result += 1000 * (invoice.audience - 30)
+        }
+        break
+
+      case 'comedy':
+        result = 30000
+        if (invoice.audience > 20) {
+          result += 10000 + 500 * (invoice.audience - 20)
+        }
+        result += 300 * invoice.audience
+        break
+
+      default:
+        throw new Error(`unknown type: ${invoice.play.type}`)
+    }
+    return result
+  }
+
+  const volumeCreditsFor = aPerformance => {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+
+    return result
+  }
+
+  const totalAmount = invoice =>
+    invoice.performances.reduce(
+      (total, aPerformance) => total + aPerformance.amount,
+      0
+    )
+
+  const totalVolumeCredits = invoice =>
+    invoice.performances.reduce(
+      (total, aPerformance) => total + aPerformance.volumeCredits,
+      0
+    )
+
+  const enrichPerformance = aPerformance => {
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(result)
+    result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
+
+    return result
+  }
+
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+
+  return statementData
+}
--- src/statement.js
+++ src/statement.js
@@ -1,70 +1,7 @@
-export const statement = (invoice, plays) => {
-  const playFor = aPerformance => plays[aPerformance.playID]
+import { createStatementData } from './createStatementData'
 
-  const amountFor = invoice => {
-    let result = 0
-    switch (invoice.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (invoice.audience > 30) {
-          result += 1000 * (invoice.audience - 30)
-        }
-        break
-
-      case 'comedy':
-        result = 30000
-        if (invoice.audience > 20) {
-          result += 10000 + 500 * (invoice.audience - 20)
-        }
-        result += 300 * invoice.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${invoice.play.type}`)
-    }
-    return result
-  }
-
-  const volumeCreditsFor = aPerformance => {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5)
-    }
-
-    return result
-  }
-
-  const totalAmount = invoice =>
-    invoice.performances.reduce(
-      (total, aPerformance) => total + aPerformance.amount,
-      0
-    )
-
-  const totalVolumeCredits = invoice =>
-    invoice.performances.reduce(
-      (total, aPerformance) => total + aPerformance.volumeCredits,
-      0
-    )
-
-  const enrichPerformance = aPerformance => {
-    const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
-
-    return result
-  }
-
-  const statementData = {}
-  statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerformance)
-  statementData.totalAmount = totalAmount(statementData)
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-
-  return renderPlainText(statementData, plays)
-}
+export const statement = (invoice, plays) =>
+  renderPlainText(createStatementData(invoice, plays))
 
 const renderPlainText = data => {
   let result = `Statement for ${data.customer}\n`
