2fb12a10b3a93f91836f57781693d5cab2a081a6
Simone Amorim <simoneas03@gmail.com>
2021-12-06 08:06:05 -0300

    Create ComedyCalculator and TragedyCalculator classes

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -42,40 +42,46 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30)
-        }
-        break
-
-      case 'comedy':
-        result = 30000
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20)
-        }
-        result += 300 * this.performance.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${this.play.type}`)
-    }
-    return result
+    throw new Error(`unknown type: ${this.play.type}`)
   }
 
   get volumeCredits() {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
 
-    if ('comedy' === this.play.type) {
-      result += Math.floor(this.performance.audience / 5)
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
     }
-
     return result
   }
 }
 
-const createPerformanceCalculator = (aPerformance, aPlay) =>
-  new PerformanceCalculator(aPerformance, aPlay)
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+}
